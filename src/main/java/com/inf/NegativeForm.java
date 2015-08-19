852fe0727434d0b413ea807603fd609b7dc2a1b7
Vladislav <vladisen1244@gmail.com>
2015-08-19 12:17:55 +0300

    2

--- src/main/java/com/inf/NegativeForm.java
+++ src/main/java/com/inf/NegativeForm.java
@@ -6,56 +6,49 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 
-/**
- * Created by v.stuparenko on 05.08.2015.
- */
 public class NegativeForm extends BasePage  {
 
 
-
-    public NegativeForm( WebDriver driver, WebElement one, WebElement two, WebElement button, WebElement acceptmMessage, Boolean blanc) throws Exception {
-        super(driver);
-    }
-
-    public NegativeForm(WebDriver driver, WebElement element) throws  Exception{
-        super(driver);
-
-    }
     public  NegativeForm( WebDriver driver, WebElement one, WebElement button, WebElement acceptMessage) throws Exception {   //тест на однострочную форму
         super(driver);
+        try{
+            verifyElementIsPresent(one);                                                //Проверяем поле
+            sendAndAssertNegativeTest(button, acceptMessage);                       //Отправка сообщения без данных
+        }catch (ElementNotFoundException e) {e.printStackTrace();}              //Еррор если чтото пошло не так
 
-            try{
-            verifyElementIsPresent(one);     //Проверяем поле
-            sendAndAssertNegativeTest(button, acceptMessage);
-            }catch (ElementNotFoundException e) {
-                e.printStackTrace();
+        try {
+            verifyElementIsPresent(one);
+            if (one.getAttribute("name").contains("phone")) {
+                setElementText(one);
+                sendAndAssertNegativeTest(button, acceptMessage);
+                one.clear();
+            }
+            if (one.getAttribute("name").contains("email")){
+               setMobileNumber(one);
+               sendAndAssertNegativeTest(button, acceptMessage);
+               one.clear();
+
             }
 
-                // кликаем и ждем месседж
-            try {
-                verifyElementIsPresent(one);
-                if (one.getAttribute("name").contains("email")) {
-                    setEmailAdress(one);
-                    sendAndAssertNegativeTest(button, acceptMessage);
-                    one.clear();
-                } else {
-                    setElementText(one);
-                    sendAndAssertNegativeTest(button, acceptMessage);
-                    one.clear();
+    }
 
 
-                    //  public void sendAndAssertNegativeTest(WebElement button, WebElement acceptMessage)  {
-                    wait.until(ExpectedConditions.elementToBeClickable(button));
-                    clickOnElement(button);
-                    try {
-                        Thread.sleep(3000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    Assert.assertTrue(!acceptMessage.isDisplayed());
-                    System.out.println("Negative test is passed");
+    public void sendAndAssertNegativeTest(WebElement button, WebElement acceptMessage)  {
+        wait.until(ExpectedConditions.elementToBeClickable(button));
+        for(int i =1 ; i<5; i++){
+            clickOnElement(button);
+        }
 
-                }
-            }catch(ElementNotFoundException e){
-            e.printStackTrace();}
-    }}
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Assert.assertTrue(!acceptMessage.isDisplayed());
+        System.out.println("Negative test is passed");
+    }
+
+
+
+
+}
